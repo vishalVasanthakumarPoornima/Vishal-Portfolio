1a0402bd48d0e78b73e94073efed4228f15b0339
Huzaif Ahmed <s>
2025-02-02 00:43:51 +0530

    fourth-commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -11,8 +11,8 @@ import framer from './tech/framer.png';
 import css from "./tech/css.png";
 import figma from "./tech/figma.png";
 import gearXpert from './gearXpert.png';
-import project3 from './project3.png';
-import project2 from './proj1.png'
+import project3 from './project3.webp';
+import project2 from './proj1.webp'
 import mysql from './tech/mysql.png';
 import express from './tech/express.png';
 import aws from './tech/aws.png';
@@ -26,9 +26,9 @@ import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import threejs from "./tech/threejs.svg";
-import firstTestimonial from './Tars Ken.jpg';
-import secondTestimonial from './image.jpg';
-import thirdTestimonial from './third testimonial.jpg'
+import firstTestimonial from './Tars Ken.webp';
+import secondTestimonial from './image.webp';
+import thirdTestimonial from './third testimonial.webp'
 
 export {
   logo,
